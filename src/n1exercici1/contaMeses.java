781042f3245a296f4fd1bd9dcd21b442a6d42bba
AnnaFedoraeva <anafedoraeva@gmail.com>
2022-07-18 10:23:51 +0200

    modified

--- src/n1exercici1/contaMeses.java
+++ src/n1exercici1/contaMeses.java
@@ -2,6 +2,9 @@ package n1exercici1;
 
 import java.util.ArrayList;
 
+//Crea una classe Java que insereixi en una llista ordenada els noms dels mesos de l'any.
+//Verifica amb jUnit que la llista té 12 posicions, que no és nul·la i que en la seva posició 8 conté el nom 'agost'.
+
 public class contaMeses {
 
 	public static void main(String[] args) {
