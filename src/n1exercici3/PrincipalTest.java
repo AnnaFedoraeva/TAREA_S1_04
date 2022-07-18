85d7d0475ceaca68db93cf5929ef9d4f6543ed15
AnnaFedoraeva <anafedoraeva@gmail.com>
2022-07-18 10:24:48 +0200

    modified

--- src/n1exercici3/PrincipalTest.java
+++ src/n1exercici3/PrincipalTest.java
@@ -10,6 +10,7 @@ class PrincipalTest {
 
 	@Test
 	void IndexOutOfBoundsException() {
+
 		ArrayList<Integer> numeros = Principal.add(1);
 
 		assertThrows(IndexOutOfBoundsException.class, () -> numeros.get(1));
